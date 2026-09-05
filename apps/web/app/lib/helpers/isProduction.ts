200938d212ba2b10fbef1390ea586803c5258bf3
Aaron Santana <84732450+Santaval@users.noreply.github.com>
2026-09-05 15:30:26 -0600

    fix(const): standardize quotes in PRODUCTION_ENV_VALUES and isProduction import

--- apps/web/app/lib/helpers/isProduction.ts
+++ apps/web/app/lib/helpers/isProduction.ts
@@ -1,4 +1,4 @@
-import { PRODUCTION_ENV_VALUES } from "../const";
+import { PRODUCTION_ENV_VALUES } from '../const'
 
 export default function isProduction(): boolean {
   return PRODUCTION_ENV_VALUES.includes(process.env.NODE_ENV as string)
