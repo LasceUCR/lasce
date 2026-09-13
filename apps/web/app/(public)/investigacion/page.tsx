8bb5d560aa9e4949b641f16e66b9abc736578ec0
josuetorres27 <josuetorres7260@gmail.com>
2026-09-12 23:47:42 -0600

    feat(publications): add researchGroup field to the publications explorer storybook

--- apps/web/app/(public)/investigacion/page.tsx
+++ apps/web/app/(public)/investigacion/page.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next'
 
 import { PublicationsExplorer } from '@/app/components/public/publications/PublicationsExplorer'
-import { ContentFlag } from '@/app/components/public/topic/ContentFlag'
 import { TopicBackLink } from '@/app/components/public/topic/TopicBackLink'
 import { TopicHero } from '@/app/components/public/topic/TopicHero'
 import {
