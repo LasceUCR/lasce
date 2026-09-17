eb56efd9d283baed69d0682c7c6f738984d74cab
josuetorres27 <josuetorres7260@gmail.com>
2026-09-17 16:48:41 -0600

    fix(publications): fix typo

--- apps/web/app/components/public/publications/PublicationsExplorer.test.tsx
+++ apps/web/app/components/public/publications/PublicationsExplorer.test.tsx
@@ -182,9 +182,7 @@ describe('PublicationsExplorer', () => {
 
     expect(screen.getAllByRole('heading', { level: 3 })).toHaveLength(filterPublications.length)
 
-    expect(screen.getByLabelText('Cantidad de publicaciones')).toHaveTextContent(
-      '2 publicaciones en total',
-    )
+    expect(screen.getByLabelText('Cantidad de publicaciones')).toHaveTextContent('2 publicaciones en total')
   })
 
   test('combines the group filter with the search query', async () => {
