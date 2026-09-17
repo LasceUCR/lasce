fa11833586678b968b932acbc3a38ea66905d8e5
josuetorres27 <josuetorres7260@gmail.com>
2026-09-17 16:55:49 -0600

    fix(publications): fix test

--- apps/web/app/components/public/publications/PublicationsExplorer.test.tsx
+++ apps/web/app/components/public/publications/PublicationsExplorer.test.tsx
@@ -182,9 +182,10 @@ describe('PublicationsExplorer', () => {
 
     expect(screen.getAllByRole('heading', { level: 3 })).toHaveLength(filterPublications.length)
 
-    expect(screen.getByLabelText('Cantidad de publicaciones')).toHaveTextContent(
-      '2 publicaciones en total',
-    )
+    const kpi = screen.getByLabelText('Cantidad de publicaciones')
+
+    expect(kpi).toHaveTextContent('2')
+    expect(kpi).toHaveTextContent('publicaciones en total')
   })
 
   test('combines the group filter with the search query', async () => {
